626725eb7abb08ab4a25d8dbfb6b5a27dddc4831
RobisBB <roberts.burnevskis@grobinasskola.lv>
2025-11-06 15:03:02 +0200

    1uzd izdarits

--- uzd1.py
+++ uzd1.py
@@ -1,7 +1,7 @@
 # Sasummē ievadītos skaitļus. 0 norāda, ka darbs jābeidz.
-summa = 0
-x = int(input("Ievadi skaitli (0 lai beigtu): "))
-while x != 0:
-    summa += x
-    x = int(input("Ievadi skaitli (0 lai beigtu): "))
-print("Summa:", summa)
+summa = 0 # inicialize summa mainigo
+x = int(input("Ievadi skaitli (0 lai beigtu): ")) # inicialize x mainigo
+while x != 0: # kamer x nav 0
+    summa += x # pievieno x summai
+    x = int(input("Ievadi skaitli (0 lai beigtu): ")) # ievadi jaunu x
+print("Summa:", summa) # izvada summu
